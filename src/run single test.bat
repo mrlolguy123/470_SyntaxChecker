9c7db6daa640f1552cfef28dd9bb5f4849d36248
Danny Le <24dannle@gmail.com>
2024-03-29 02:47:09 -0400

    Delete run single test.bat
    fuck you

--- src/run single test.bat
+++
@@ -1,9 +0,0 @@
-del *.class
-java -jar jflex-1.6.1.jar Lexer.flex
-javac *.java
-
-java Program ../samples/fail_01.minc   > ../samples/output_fail_01.txt
-
-java Program ../samples/succ_01.minc   > ../samples/output_succ_01.txt
-
-PAUSE
